34770a07a03110f777070b7c9f38919ac003eb5b
Zaijen Santos <santoz2@uw.edu>
2018-12-02 17:48:57 -0800

    Add data_reformatting file

---
+++ data_reformatter.R
@@ -0,0 +1,15 @@
+library(ggplot2)
+library(dplyr)
+## This file takes the data formatted as 3 rows, and flips it,
+## making a dataframe of 3 columns. The output file is reformatted_data.csv
+
+trip_data2 <- read.csv("data/tripData.csv", stringsAsFactors = FALSE, header = FALSE)
+df_transpose <- t(trip_data2)
+colnames(df_transpose) = df_transpose[1, ]
+df_transpose = df_transpose[-1, ]
+final_frame <- data.frame(df_transpose, stringsAsFactors = FALSE)
+correct_classes <- final_frame %>% mutate(Yellow = as.numeric(Yellow)) %>% mutate(FHV = as.numeric(FHV)) %>%
+  mutate(transportType = as.Date(transportType))
+reformat <- correct_classes %>% rename(Date = transportType)
+
+write.csv(reformat, "reformatted_data.csv", row.names = FALSE)
