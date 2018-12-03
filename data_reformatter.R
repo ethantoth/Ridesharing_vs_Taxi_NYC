8be952698c850f8efb8dda1734533df65162db3f
Zaijen Santos <santoz2@uw.edu>
2018-12-02 17:51:17 -0800

    Add data reformmatting file

--- data_reformatter.R
+++ data_reformatter.R
@@ -11,5 +11,4 @@ final_frame <- data.frame(df_transpose, stringsAsFactors = FALSE)
 correct_classes <- final_frame %>% mutate(Yellow = as.numeric(Yellow)) %>% mutate(FHV = as.numeric(FHV)) %>%
   mutate(transportType = as.Date(transportType))
 reformat <- correct_classes %>% rename(Date = transportType)
-
 write.csv(reformat, "reformatted_data.csv", row.names = FALSE)
